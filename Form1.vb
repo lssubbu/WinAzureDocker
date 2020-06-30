9723b1471072002ebcbba38f56c3920d0df1989a
lss <lssubbu@gmail.com>
2020-06-30 07:28:39 +0530

    7.28

--- Form1.vb
+++ Form1.vb
@@ -1,5 +1,5 @@
 ﻿Public Class Form1
-    'comment 300620 7.21
+    'comment 300620 7.28
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
         MsgBox(TextBox1.Text + TextBox2.Text)
